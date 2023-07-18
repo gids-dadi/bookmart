c8d4f6438aecc123fcdd6526812636807ca833c6
Gideon <gideons564@gmail.com>
2023-07-18 02:46:02 +0100

    Add cors and database

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
-// Receive and perse json file
-app.use(express.json);
+// Event logger
+app.use(logger);
+
+// enable cors
+app.use(cors(corsOptions));
+
+// Receive and parse json request body
+app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // Setup path for static css file
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -23,5 +38,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 not found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
